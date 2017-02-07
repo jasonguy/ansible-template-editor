42b413a5fd6fc9254253d426418546d71d083fb4
Jason Guy <jguy@cumulusnetworks.com>
2017-02-07 12:34:49 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Ansible and Jinja2 editor/debugger/renderer
+
+A simple web-based app to edit, debug, and render ansible templates. 
+
+This tool is forked and extended from the [jinja2-live-parser](https://github.com/jasonguy/jinja2-live-parser), which 
+is a lightweight live parser for [Jinja2](http://jinja.pocoo.org/docs/dev/) based on [Flask](http://flask.pocoo.org/)  
+and [Jquery](http://jquery.com/).  
+
+## Install
+
+### Clone + pip
+
+    $ git clone https://github.com/jasonguy/ansible-template-editor.git
+    $ pip install -r requirements.txt
+    $ python parser.py
+
+## Usage
+
+You are all set, browse to `http://localhost:5000/` and paste in your ansible template code and variables.  
+You can add any custom filter you'd like in `filters.py`.  Just make sure the function's name starts with `filter_`.
---
+++ parser.py
@@ -0,0 +1,125 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, print_function
+
+from flask import Flask, render_template, request
+from jinja2 import Environment, meta, exceptions
+from random import choice
+from inspect import getmembers, isfunction
+from cgi import escape
+import logging
+import logging.handlers
+import json
+import yaml
+import config
+
+
+app = Flask(__name__)
+
+
+def get_custom_filters():
+    import filters
+    custom_filters = {}
+    for m in getmembers(filters):
+        if m[0].startswith('filter_') and isfunction(m[1]):
+            filter_name = m[0][7:]
+            custom_filters[filter_name] = m[1]
+
+    return custom_filters
+
+
+def get_ansible_filters():
+    from  ansible.plugins.filter import core, ipaddr, mathstuff
+    ansible_filters = {}
+    for name,value in getmembers(core):
+        if not name.startswith('_') and isfunction(value):
+            ansible_filters[name] = value
+
+    for name,value in getmembers(ipaddr):
+        if not name.startswith('_') and isfunction(value):
+            ansible_filters[name] = value
+
+    for name,value in getmembers(mathstuff):
+        if not name.startswith('_') and isfunction(value):
+            ansible_filters[name] = value
+
+    return ansible_filters
+
+
+@app.route("/")
+def home():
+    return render_template('index.html', custom_filters=get_custom_filters())
+
+
+@app.route('/convert', methods=['GET', 'POST'])
+def convert():
+    jinja2_env = Environment()
+
+    # Load ansible filters
+    ansible_filters = get_ansible_filters()
+    app.logger.debug('Add the following ansible filters to Jinja environment: %s' % ', '.join(ansible_filters.keys()))
+    jinja2_env.filters.update(ansible_filters)
+
+    # Load custom filters
+    custom_filters = get_custom_filters()
+    app.logger.debug('Add the following customer filters to Jinja environment: %s' % ', '.join(custom_filters.keys()))
+    jinja2_env.filters.update(custom_filters)
+
+    # Load the template
+    try:
+        jinja2_tpl = jinja2_env.from_string(request.form['template'])
+    except (exceptions.TemplateSyntaxError, exceptions.TemplateError) as e:
+        return "Syntax error in jinja2 template: {0}".format(e)
+
+
+    dummy_values = [ 'Lorem', 'Ipsum', 'Amet', 'Elit', 'Expositum',
+        'Dissimile', 'Superiori', 'Laboro', 'Torquate', 'sunt',
+    ]
+    values = {}
+    if bool(int(request.form['dummyvalues'])):
+        # List template variables (introspection)
+        vars_to_fill = meta.find_undeclared_variables(jinja2_env.parse(request.form['template']))
+
+        for v in vars_to_fill:
+            values[v] = choice(dummy_values)
+    else:
+        # Check JSON for errors
+        if request.form['input_type'] == "json":
+            try:
+                values = json.loads(request.form['values'])
+            except ValueError as e:
+                return "Value error in JSON: {0}".format(e)
+        # Check YAML for errors
+        elif request.form['input_type'] == "yaml":
+            try:
+                values = yaml.load(request.form['values'])
+            except (ValueError, yaml.parser.ParserError, TypeError) as e:
+                return "Value error in YAML: {0}".format(e)
+        else:
+            return "Undefined input_type: {0}".format(request.form['input_type'])
+
+    # If ve have empty var array or other errors we need to catch it and show
+    try:
+        rendered_jinja2_tpl = jinja2_tpl.render(values)
+    except (ValueError, TypeError) as e:
+        return "Error in your values input filed: {0}".format(e)
+
+    if bool(int(request.form['showwhitespaces'])):
+        # Replace whitespaces with a visible character (will be grayed with javascript)
+        rendered_jinja2_tpl = rendered_jinja2_tpl.replace(' ', u'•')
+
+    return escape(rendered_jinja2_tpl).replace('\n', '<br />')
+
+
+if __name__ == "__main__":
+    # Set up logging
+    app.logger.setLevel(logging.__getattribute__(config.LOGGING_LEVEL))
+    file_handler = logging.handlers.RotatingFileHandler(filename=config.LOGGING_LOCATION, maxBytes=10*1024*1024, backupCount=5)
+    file_handler.setFormatter(logging.Formatter(config.LOGGING_FORMAT))
+    file_handler.setLevel(logging.__getattribute__(config.LOGGING_LEVEL))
+    app.logger.addHandler(file_handler)
+
+    app.run(
+        host=config.HOST,
+        port=config.PORT,
+        debug=config.DEBUG,
+    )
